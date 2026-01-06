8aaade8c3239a33f5c20a71ec7028e09ea5ac999
jnadyr <jnadyr@gmail.com>
2026-01-06 20:21:09 -0300

    Ansible entrando na jogada

--- main.tf
+++ main.tf
@@ -17,7 +17,14 @@ provider "aws" {
 resource "aws_instance" "app_server" {
   ami           = "ami-0f5fcdfbd140e4ab7"
   instance_type = "t3.micro"
+  #user_data = <<-EOF
+  #            #!/bin/bash
+  #            cd /home/ubuntu
+  #            echo "<h1>Segunda Maquina</h1>" > index.html
+  #            nohup busybox httpd -f -p 8080 &
+  #            EOF
   key_name      = "IaC-ParDeChaves"
+
 
   tags = {
     Name = "Primeira Instancia com Terraform"
